4514b567b8d18dedbd91ba122a0e151e7e0b5625
Nicole <106133672+rvnicole@users.noreply.github.com>
2023-02-02 04:32:25 +0000

    Validando variable de entorno

--- myApp.js
+++ myApp.js
@@ -1,23 +1,31 @@
 let express = require('express');
 let app = express();
 
-console.log( "Hello World");
+console.log("Hello World");
 
-app.get( "/hola", ( req, res ) => {
-    res.send( "Hello Express" );
+app.get("/hola", (req, res) => {
+  res.send("Hello Express");
 });
 
-app.get( "/", ( req, res ) => {
-    res.sendFile( __dirname + "/views/index.html" );
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-app.use( "/public", express.static( __dirname + "/public" ) );
+app.use("/public", express.static(__dirname + "/public"));
 
-app.get( "/json", ( req, res ) => {
-    res.json( { "message": "Hello json" } );
+app.get("/json", (req, res) => {
+  let cadena = "Hello json";
+
+  const mySecret = process.env['MESSAGE_STYLE'];
+
+  if (mySecret === "uppercase") {
+    cadena = "Hello json".toUpperCase();
+  }
+
+  res.json({ "message": cadena });
 });
 
-process.env.MESSAGE_STYLE=uppercase;
+//process.env.MESSAGE_STYLE=uppercase;
 
 
 
@@ -50,4 +58,4 @@ process.env.MESSAGE_STYLE=uppercase;
 
 
 
- module.exports = app;
+module.exports = app;
