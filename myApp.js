4e3c438e43483f3cb6ec823fa3c12834eb3d3b2d
Nicole <ceinnova05476@innovaccion.mx>
2023-02-01 21:57:37 -0600

    Sirviendo un json

--- myApp.js
+++ myApp.js
@@ -13,7 +13,9 @@ app.get( "/", ( req, res ) => {
 
 app.use( "/public", express.static( __dirname + "/public" ) );
 
-
+app.get( "/json", ( req, res ) => {
+    res.json( { "message": "Hello json" } );
+})
 
 
 
