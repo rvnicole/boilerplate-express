3902275e04ac72c3d359f9b1874f4df9009a3dbd
Nicole <ceinnova05476@innovaccion.mx>
2023-02-01 21:21:50 -0600

    Correccion archivos estaticos

--- myApp.js
+++ myApp.js
@@ -11,7 +11,7 @@ app.get( "/", ( req, res ) => {
     res.sendFile( __dirname + "/views/index.html" );
 });
 
-app.use( express.static( __dirname + "/public" ) );
+app.use( "/public", express.static( __dirname + "/public" ) );
 
 
 
