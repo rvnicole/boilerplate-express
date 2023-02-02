b7e8e264ce1396f2a40d43288f582f45d2788e08
Nicole <ceinnova05476@innovaccion.mx>
2023-02-01 22:56:52 -0600

    Probando app.use correccion 2

--- myApp.js
+++ myApp.js
@@ -27,7 +27,7 @@ app.get("/json", (req, res) => {
 
 //process.env.MESSAGE_STYLE=uppercase;
 
-app.use( function( req, res, next ){
+app.use( "/json", function( req, res, next ){
   console.log( req );
   console.log( req.method + " " + req.path + " - " + req.ip );
   next();
