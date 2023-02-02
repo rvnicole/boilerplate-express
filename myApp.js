24184015b2f413f6eaa9212af86a6282d893ed44
Nicole <ceinnova05476@innovaccion.mx>
2023-02-01 20:51:47 -0600

    Corregir respuesta

--- myApp.js
+++ myApp.js
@@ -3,13 +3,14 @@ let app = express();
 
 console.log( "Hello World");
 
-app.get( "/", ( req, res ) => {
+app.get( "/hola", ( req, res ) => {
     res.send( "Hello Express" );
 });
 
 app.get( "/", ( req, res ) => {
     res.sendFile( __dirname + "/views/index.html" );
 });
+
 
 
 
