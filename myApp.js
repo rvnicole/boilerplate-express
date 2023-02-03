4e14a6438217a004ff077f9270ecef3b047c1b3b
Nicole <ceinnova05476@innovaccion.mx>
2023-02-02 18:19:39 -0600

    Servir el tiempo

--- myApp.js
+++ myApp.js
@@ -34,6 +34,17 @@ app.get("/json", (req, res) => {
   res.json({ "message": cadena });
 });
 
+app.get( "/now", ( req, res, next ) => {
+
+    req.time = ( new Date() ).toString();
+    next();
+
+}, ( req, res ) => {
+
+    res.send( { time : req.time } );
+
+});
+
 
 
 
