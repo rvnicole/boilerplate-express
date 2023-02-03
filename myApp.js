dc6bc7abf0249ec029132db84a96f79d2fae8c61
Nicole <ceinnova05476@innovaccion.mx>
2023-02-02 21:33:16 -0600

    body-parser

--- myApp.js
+++ myApp.js
@@ -1,6 +1,10 @@
 let express = require('express');
 let app = express();
 
+let bodyParser = require( 'body-parser' );
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
 console.log("Hello World");
 
 app.get("/hola", (req, res) => {
